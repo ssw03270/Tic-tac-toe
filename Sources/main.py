dee11a8cfcc987382a87f4ef168af1040b38692e
ssw03270 <ttd8591@gmail.com>
2021-02-23 12:36:17 +0900

    [Fix] - Fix overlaping mark

--- Sources/main.py
+++ Sources/main.py
@@ -18,68 +18,81 @@ def drawCircle(x : int, y : int):
     pygame.draw.circle(screen, RED, [x + 50, y + 50], 35, 2)   # divide boundaries
 # draw game result
 def drawResult(winner : int):
+    # draw result box
     pygame.draw.rect(screen, WHITE, [50, 100, 275, 200])
-    pygame.draw.aalines(screen, BLACK, True, [[50, 100], [50, 300], [350, 300], [350, 100]], True)
+    pygame.draw.aalines(screen, BLACK, True, [[50, 100], [50, 300], [325, 300], [325, 100]], True)
 
+    # set font
     font = pygame.font.SysFont('arial',30)  #폰트 설정
 
+    # set text 
     textStr = "X is winner" if winner == 1 else "O is winner"
     textStr2 = "If you want to play again"
     textStr3 = "press R"
 
+    # set text render
     text = font.render(textStr, True, BLACK)
     text2 = font.render(textStr2, True, BLACK)
     text3 = font.render(textStr3, True, BLACK)
 
+    # set text position
     textRect = text.get_rect(center = [200, 155])
     textRect2 = text2.get_rect(center = [200, 210])
     textRect3 = text3.get_rect(center = [200, 240])
 
+    # show text in display
     screen.blit(text,textRect)
     screen.blit(text2,textRect2)
     screen.blit(text3,textRect3)
 # check mouse pos and draw mark
-def checkMousePos(mousePos : tuple):
+# return bool type, can mark -> return true
+def normalizeMousePos(mousePos : tuple)-> bool:
+    # get mouse position
     x = mousePos[0]
     y = mousePos[1]
     
-    if 50 < x and x < 150:
-        if 50 < y and y < 150:
+    # normalize mouse position
+    if 50 < x and x < 150:          # if 50 < x < 150
+        if 50 < y and y < 150:      # if 50 < y < 150
             x = 50
             y = 50
-        elif 150 < y and y < 250:
+        elif 150 < y and y < 250:   # if 150 < y < 250
             x = 50
             y = 150
-        elif 250 < y and y < 350:
+        elif 250 < y and y < 350:   # if 250 < y < 350
             x = 50
             y = 250
-    elif 150 < x and x < 250:
-        if 50 < y and y < 150:
+    elif 150 < x and x < 250:       # if 150 < x < 250
+        if 50 < y and y < 150:      # if 50 < y < 150
             x = 150
             y = 50
-        elif 150 < y and y < 250:
+        elif 150 < y and y < 250:   # if 150 < y < 250
             x = 150
             y = 150
-        elif 250 < y and y < 350:
+        elif 250 < y and y < 350:   # if 250 < y < 350
             x = 150
             y = 250
-    elif 250 < x and x < 350:
-        if 50 < y and y < 150:
+    elif 250 < x and x < 350:       # if 250 < x < 350
+        if 50 < y and y < 150:      # if 50 < y < 150
             x = 250
             y = 50
-        elif 150 < y and y < 250:
+        elif 150 < y and y < 250:   # if 150 < y < 250
             x = 250
             y = 150
-        elif 250 < y and y < 350:
+        elif 250 < y and y < 350:   # if 250 < y < 350
             x = 250
             y = 250
 
-    if(isPlayerOne):
-        drawX(x, y)
-        map[int((x - 50) / 100)][int((y - 50) / 100)] = 1
+    if(map[int((x - 50) / 100)][int((y - 50) / 100)] == 0):  # if map[x][y] hasn't mark
+        if(isPlayerOne):                # if player one win
+            drawX(x, y)                 # draw x
+            map[int((x - 50) / 100)][int((y - 50) / 100)] = 1
+        else:                           # if player two win
+            drawCircle(x, y)            # draw circle
+            map[int((x - 50) / 100)][int((y - 50) / 100)] = 2
+        return True
     else:
-        drawCircle(x, y)
-        map[int((x - 50) / 100)][int((y - 50) / 100)] = 2
+        return False
 # check who is winner
 def checkWinner():
     winner = 0
@@ -101,7 +114,7 @@ def checkWinner():
         if isFinishO:
             winner = 2
 
-    if winner != 0:
+    if winner != 0: # if there is a winner
         return winner
         
     # check Horizontal
@@ -124,7 +137,7 @@ def checkWinner():
             winner = 2
             break
 
-    if winner != 0:
+    if winner != 0: # if there is a winner
         return winner
 
     
@@ -165,12 +178,13 @@ def checkWinner():
     return winner
 # restart game
 def restartGame():
-    global end
+    global end     
     end = False
     global map
-    map = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-    drawInit()
+    map = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]         # reset map
+    drawInit()                                      # draw game again
 
+# pygame start
 pygame.init()
 
 # game color
@@ -190,37 +204,36 @@ pygame.display.set_caption("Tic-tac-toe")
 mouseDown = False
 end = False
 done = False
-
-map = [[0, 0, 0], [0, 0, 0], [0, 0, 0]] # 0 : blank, 1 : x, 2 : o
-isPlayerOne = True  # True : playerOne, False : playerTwo
-clock = pygame.time.Clock()
+map = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]             # 0 : blank, 1 : x, 2 : o
+isPlayerOne = True                                  # True : playerOne, False : playerTwo
+clock = pygame.time.Clock()                         # pygame set fps value
 
 # set game
 drawInit()
 
 # loop game
 while not done:
-    clock.tick(10)  # set fps, 1 sec to 10 frame
+    clock.tick(10)                                  # set fps, 1 sec to 10 frame
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:   # if quit game
+        if event.type == pygame.QUIT:               # if quit game
             done = True
-        if event.type == pygame.MOUSEBUTTONDOWN: # if input mouse button down
+        if event.type == pygame.MOUSEBUTTONDOWN:    # if input mouse button down
             mouseDown = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == ord('r'):
-                restartGame()
+        if event.type == pygame.KEYDOWN:            # if input key down
+            if event.key == ord('r'):               # if input key down "R"
+                restartGame()                       # restart game
   
-    if mouseDown and not end:
-        mousePos = pygame.mouse.get_pos()
-        checkMousePos(mousePos)
-        isPlayerOne = not isPlayerOne
-        winner = checkWinner()
-        if winner != 0:
-            drawResult(winner)
-            end = True
+    if mouseDown and not end:                       # if mouse down and game wasn't end
+        mousePos = pygame.mouse.get_pos()           # get mouse position    
+        if(normalizeMousePos(mousePos)):            # normalization mouse position
+            isPlayerOne = not isPlayerOne           # turn player
+            winner = checkWinner()                  # check winner
+            if winner != 0:                         # if there is a winner
+                drawResult(winner)                  # draw result
+                end = True                          # game end
 
-    pygame.display.flip()   # update game display 
+    pygame.display.flip()                           # update game display 
 
     # update value
     mouseDown = False
